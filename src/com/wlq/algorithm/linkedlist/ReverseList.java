b38c243b6de9d86a89d0efb7611fa37bf253c41b
wlq1005 <793829749@qq.com>
2019-11-22 22:48:03 +0800

    反转链表

--- src/com/wlq/algorithm/linkedlist/ReverseList.java
+++ src/com/wlq/algorithm/linkedlist/ReverseList.java
@@ -1,6 +1,9 @@
 package com.wlq.algorithm.linkedlist;
 
 /**
+ * 206. 反转链表
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ *
  * @author wuliuqing
  * @date 2019/11/22 14:13
  **/
@@ -10,12 +13,14 @@ public class ReverseList {
         ListNode temp = head;
         ListNode result = null;
         while (temp != null) {
-            if (result != null) {
+            if (result == null) {
+                result = new ListNode(temp.val);
+            } else {
                 ListNode node = new ListNode(temp.val);
-                result.next = node;
+                node.next = result;
+                result = node;
             }
             temp = temp.next;
-            result = new ListNode(temp.val);
         }
         return result;
     }
