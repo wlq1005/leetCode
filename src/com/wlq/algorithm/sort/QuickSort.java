8127f185e67fd0cb12093ef41186fc23719e9051
793829749 <793829749@qq.com>
2020-01-07 18:22:38 +0800

    常见排序-快速排序

--- src/com/wlq/algorithm/sort/QuickSort.java
+++ src/com/wlq/algorithm/sort/QuickSort.java
@@ -1,5 +1,7 @@
 package com.wlq.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * 快速排序
  *
@@ -7,4 +9,15 @@ package com.wlq.algorithm.sort;
  * @date 2020/1/7 17:17
  **/
 public class QuickSort {
+
+    private void sort(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        int[] arr = new int[]{6, 7, 8, 10, 1, 3};
+        quickSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
