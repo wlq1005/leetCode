3e9ffd4784904a288d3623f21331aa74e9399f95
793829749 <793829749@qq.com>
2020-01-19 16:42:47 +0800

    买卖股票的最佳时机

--- src/com/wlq/algorithm/dynamic/BestTimeToBuyAndSellStock.java
+++ src/com/wlq/algorithm/dynamic/BestTimeToBuyAndSellStock.java
@@ -13,6 +13,9 @@ public class BestTimeToBuyAndSellStock {
         if (prices.length == 0) {
             return 0;
         }
+        // 只考需虑当天最少的买入和最大的卖出
+        // 最大买入:buy = max(buy, -price[i])
+        // 最大卖出:sell = max(sell, buy + price[i])
         int buy = -prices[0];
         int sell = 0;
         for (int i = 1; i < prices.length; i++) {
