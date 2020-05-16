898cd90e77e9a0cc4863eb0732b4692036160c7b
wlq1005 <793829749@qq.com>
2020-05-16 22:56:08 +0800

    560. 和为K的子数组

--- src/com/wlq/algorithm/array/SubarraySumEqualsK.java
+++ src/com/wlq/algorithm/array/SubarraySumEqualsK.java
@@ -12,11 +12,8 @@ public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            int sum = nums[i];
-            if (sum == k) {
-                count++;
-            }
-            for (int j = i + 1; j < nums.length; j++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
                 sum += nums[j];
                 if (sum == k) {
                     count++;
