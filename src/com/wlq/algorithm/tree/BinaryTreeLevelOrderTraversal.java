53ba03c3a7340db06098ff2681fd799589560e0a
wlq1005 <793829749@qq.com>
2020-06-03 23:56:19 +0800

    DFS

--- src/com/wlq/algorithm/tree/BinaryTreeLevelOrderTraversal.java
+++ src/com/wlq/algorithm/tree/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,8 @@
 package com.wlq.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 102. 二叉树的层序遍历
  * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
@@ -8,4 +11,36 @@ package com.wlq.algorithm.tree;
  * @since 2020/5/13 00:07
  */
 public class BinaryTreeLevelOrderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        dfs(1, root, result);
+        return result;
+    }
+
+    public void dfs(int index, TreeNode node, List<List<Integer>> result) {
+        if (result.size() < index) {
+            result.add(new ArrayList<>());
+        }
+        result.get(index - 1).add(node.val);
+        if (node.left != null) {
+            dfs(index + 1, node.left, result);
+        }
+        if (node.right != null) {
+            dfs(index + 1, node.right, result);
+        }
+    }
 }
