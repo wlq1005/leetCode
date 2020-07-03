9e337520903989e93676f693145a442d8c6ba1dd
793829749 <793829749@qq.com>
2020-07-03 19:58:09 +0800

    stash

---
+++ src/com/wlq/algorithm/tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,29 @@
+package com.wlq.algorithm.tree;
+
+/**
+ * 108. 将有序数组转换为二叉搜索树
+ * https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
+ *
+ * @author wuliuqing
+ * @date 2020/7/3 19:31
+ **/
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
